95c86be6bad05cd59aad1e2c09e9d8792b57e522
Elsaamelia55 <145968579+Elsaamelia55@users.noreply.github.com>
2023-12-25 23:35:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+   function tampilgambar() {
+      const imagePicker = document.getElementById('imagePicker');
+      const select = imagePicker.value;
+      const tampilan = document.getElementById('tampilan');
+      const fotosatu = select + ".jpg";
+      const fotodua = select + ".jpg";
+      const fototiga = select + ".jpg";
+      const fotoempat = select + ".jpg";
+      const fotolima = select + ".jpg";
+      const fotoenam = select + ".jpg";
+  }
+  
